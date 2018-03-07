a34843e9de4e983fe6887fab4b4f10e3b05120dd
iroro orife <iorife@marchex.com>
2018-03-06 19:50:20 -0800

    [ADD] updates for TF transformer

--- hyperparams.py
+++ hyperparams.py
@@ -8,10 +8,10 @@ https://www.github.com/kyubyong/transformer
 class Hyperparams:
     '''Hyperparameters'''
     # data
-    source_train = '/Users/iorife/github/yoruba_asr/automatic_diacritization/seq2seq/nmt_data/yoruba_diacritics/train/sources.txt'
-    target_train = '/Users/iorife/github/yoruba_asr/automatic_diacritization/seq2seq/nmt_data/yoruba_diacritics/train/targets.txt'
-    source_test = '/Users/iorife/github/yoruba_asr/automatic_diacritization/seq2seq/nmt_data/yoruba_diacritics/dev/sources.txt'
-    target_test = '/Users/iorife/github/yoruba_asr/automatic_diacritization/seq2seq/nmt_data/yoruba_diacritics/dev/targets.txt'
+    source_train = '/home/iorife/github/yoruba_asr/automatic_diacritization/seq2seq/nmt_data/yoruba_diacritics/train/sources.txt'
+    target_train = '/home/iorife/github/yoruba_asr/automatic_diacritization/seq2seq/nmt_data/yoruba_diacritics/train/targets.txt'
+    source_test = '/home/iorife/github/yoruba_asr/automatic_diacritization/seq2seq/nmt_data/yoruba_diacritics/dev/sources.txt'
+    target_test = '/home/iorife/github/yoruba_asr/automatic_diacritization/seq2seq/nmt_data/yoruba_diacritics/dev/targets.txt'
     
     # training
     batch_size = 32 # alias = N
@@ -19,12 +19,12 @@ class Hyperparams:
     logdir = 'logdir' # log directory
     
     # model
-    maxlen = 10 # Maximum number of words in a sentence. alias = T.
+    maxlen = 30 # Maximum number of words in a sentence. alias = T.
                 # Feel free to increase this if you are ambitious.
-    min_cnt = 20 # words whose occurred less than min_cnt are encoded as <UNK>.
+    min_cnt = 1 # words whose occurred less than min_cnt are encoded as <UNK>.
     hidden_units = 512 # alias = C
     num_blocks = 6 # number of encoder/decoder blocks
-    num_epochs = 20
+    num_epochs = 200
     num_heads = 8
     dropout_rate = 0.1
     sinusoid = False # If True, use sinusoid. If false, positional embedding.
